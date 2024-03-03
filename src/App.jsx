bd8d20678b7d076ce5e042038879dbd05897fb73
AndSim <siminiucandrew@gmail.com>
2024-03-03 15:56:36 +0200

    feat: create Reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import CreateCustomer from './CreateCustomer.jsx';
+import Customer from './Customer.jsx';
+import AccountOperations from './AccountOperations.jsx';
+import BalanceDisplay from './BalanceDisplay.jsx';
 import './App.css';
 
 export default function App() {
-	return <h1>The React-Redux Bank</h1>;
+	return (
+		<div>
+			<h1>🏦 The React-Redux Bank ⚛️</h1>
+			<CreateCustomer />
+			<Customer />
+			<AccountOperations />
+			<BalanceDisplay />
+		</div>
+	);
 }
--- src/store.jsx
+++ src/store.jsx
@@ -1,3 +1,5 @@
+import { createStore } from 'redux';
+
 const initialState = {
 	balance: 0,
 	loan: 0,
@@ -14,12 +16,39 @@ function reducer(state = initialState, action) {
 
 		case 'account/requestLoan':
 			if (state.loan > 0) return state;
-			return { ...state, loan: action.payload };
+			return {
+				...state,
+				loan: action.payload.amount,
+				loanPurpose: action.payload.purpose,
+				balance: state.balance + action.payload.amount,
+			};
 
 		case 'account/payLoan':
-			return { ...state, loan: 0, loanPurpose: '', balance: state.balance - state.loan };
+			return {
+				...state,
+				loan: 0,
+				loanPurpose: '',
+				balance: state.balance - state.loan,
+			};
 
 		default:
 			return state;
 	}
 }
+
+const store = createStore(reducer);
+
+store.dispatch({ type: 'account/deposit', payload: 500 });
+console.log(store.getState());
+
+store.dispatch({ type: 'account/withdraw', payload: 200 });
+console.log(store.getState());
+
+store.dispatch({
+	type: 'account/requestLoan',
+	payload: { amount: 1000, purpose: 'Buy a car' },
+});
+console.log(store.getState());
+
+store.dispatch({ type: 'account/payLoan' });
+console.log(store.getState());
